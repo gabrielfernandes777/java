18a0d6226583ed2e2cf01804c878ae93e8778d9b
gabriel gostoso <128156868+gabrielfernandes777@users.noreply.github.com>
2024-10-31 08:14:57 -0300

    .

---
+++ main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse e o Streen Match");
+        System.out.println("Filme: Velozes e Furiosos");
+
+        int ano = 2022;
+        boolean incluedoNoPlano = true;
+        double notaFilme = 10.0;
+
+        System.out.println("Ano de lancamento " + ano);
+    }
+}
